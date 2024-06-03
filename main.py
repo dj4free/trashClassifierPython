64f147ae55fa1409955d4d63d1b8c767a3b733ef
David Cummings <dcumm80@wgu.edu>
2024-06-03 14:25:53 -0700

    Implemented basic GUI and added dataset and logo

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+from taipy.gui import Gui
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+logo_path = "elements/WasteWise.png"
+content = ""
 
+index = """
+<|text-center|
+<|{logo_path}|image|>
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+<|{content}|file_selector|>
+select an image from your file system
+>
+"""
 
+app = Gui(page=index)
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # use_reloader enables automatic reloading
+    app.run(use_reloader=True)
